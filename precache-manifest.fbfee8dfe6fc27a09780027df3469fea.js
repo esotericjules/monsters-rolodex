e621fb45c1b85775d4974d9300f5a675039f338b
Juliet Onyekaoha <juliet.onyekaoha@venturegargengroup.com>
2020-05-26 13:23:24 +0100

    Updates

---
+++ precache-manifest.fbfee8dfe6fc27a09780027df3469fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231832b7646e502387c01b542c5b9e4d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "263eaf85fc41a0a461f9",
+    "url": "/monsters-rolodex/static/css/main.765d8162.chunk.css"
+  },
+  {
+    "revision": "eb1d9a75c88570f4e746",
+    "url": "/monsters-rolodex/static/js/2.feaeb0f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.feaeb0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263eaf85fc41a0a461f9",
+    "url": "/monsters-rolodex/static/js/main.729097c1.chunk.js"
+  },
+  {
+    "revision": "46fd975147127cca9dfe",
+    "url": "/monsters-rolodex/static/js/runtime-main.fb6124d2.js"
+  }
+]);
